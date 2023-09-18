be596e1b28087fdd881f15e1354bb2cb886f277e
Alexey Zatonskiy <lehazm.z@gmail.com>
2023-09-19 00:57:10 +0300

    add varios chek path

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,8 +1,18 @@
 from dotenv import load_dotenv
 import os
 
+def chek_variable(value)-> str | None:
+    load_dotenv()
+    value = os.environ.get("DB_USER")
 
-load_dotenv()
+    if value is None:
+        return 'src/settings'
+    
+    return None
+
+DB_USER = ''
+
+load_dotenv(chek_variable(DB_USER))
 
 DB_USER = os.environ.get("DB_USER")
 DB_HOST = os.environ.get("DB_HOST")
