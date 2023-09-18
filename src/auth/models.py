7113ff04042f6b804dadae5e40702b10fac0892f
Alexey Zatonskiy <lehazm.z@gmail.com>
2023-09-19 00:53:16 +0300

    new user model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy import Integer, MetaData, Column, String, UUID, ForeignKey, DateTime, Tuple
+from sqlalchemy import Integer, MetaData, Column, String, UUID, ForeignKey, TIMESTAMP, Boolean
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
+
 
 from uuid import uuid4
 from datetime import datetime
@@ -10,11 +12,16 @@ metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
 
-class User(Base):
+class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = 'users'
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name = Column(String)
-    mail = Column(String)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    first_name = Column(String)
+    lust_name = Column(String)
+    email = Column(String)
     phone = Column(String)
-    registrated_at = Column(DateTime, default = datetime.utcnow)
+    registrated_at = Column(TIMESTAMP, default = datetime.utcnow)
+    hashed_password = Column(String(length=1024))
+    is_active = Column(Boolean)
+    is_superuser = Column(Boolean)
+    is_verified = Column(Boolean)
